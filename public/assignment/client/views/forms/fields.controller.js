e8b73e4df91ea11fa0a0fa48aff3fd6baa676370
karthikchandranna <karthik@ccs.neu.edu>
2016-03-18 11:08:19 -0400

    add new fields

--- public/assignment/client/views/forms/fields.controller.js
+++ public/assignment/client/views/forms/fields.controller.js
@@ -5,21 +5,63 @@
         .controller("FieldController", FieldController);
     function FieldController() {
         var vm = this;
+        vm.addField = addField;
         vm.fields = [
-            {label:"TEXT", placeholder:"TEXT", fieldType:"TEXT"},
-            {label:"TEXTAREA", placeholder:"TEXTAREA", fieldType:"TEXTAREA"},
-            {label:"DATE", placeholder:"DATE", fieldType:"DATE"},
-            {label:"DROPDOWN", placeholder:"DROPDOWN", fieldType:"DROPDOWN",
+            {label:"TEXT", placeholder:"TEXT", type:"TEXT"},
+            {label:"TEXTAREA", placeholder:"TEXTAREA", type:"TEXTAREA"},
+            {label:"DATE", placeholder:"DATE", type:"DATE"},
+            {label:"OPTIONS", placeholder:"OPTIONS", type:"OPTIONS",
                 options:[
                     {"label": "Option 1", "value": "OPTION_1"},
                     {"label": "Option 2", "value": "OPTION_2"}]},
-            {label:"CHECKBOX", placeholder:"CHECKBOX", fieldType:"CHECKBOX",
+            {label:"CHECKBOXES", placeholder:"CHECKBOXES", type:"CHECKBOXES",
                 options:[
                     {"label": "Option 1", "value": "OPTION_1"},
                     {"label": "Option 2", "value": "OPTION_2"}]},
-            {label:"RADIO", placeholder:"RADIO", fieldType:"RADIO",
+            {label:"RADIOS", placeholder:"RADIOS", type:"RADIOS",
                 options:[
                     {"label": "Option 1", "value": "OPTION_1"},
-                    {"label": "Option 2", "value": "OPTION_2"}]}]
+                    {"label": "Option 2", "value": "OPTION_2"}]}];
+
+        function addField(fieldType) {
+            if (!fieldType) return;
+            var field = {"type":fieldType};
+            switch(fieldType) {
+                case "TEXT":
+                case "TEXTAREA":
+                    field.label="New Text Field";
+                    field.placeholder="New Text Field";
+                    break;
+                case "DATE":
+                    field.label = "New Date Field";
+                    break;
+                case "OPTIONS":
+                    field.label="New Dropdown";
+                    field.options=[
+                        {"label": "Option 1", "value": "OPTION_1"},
+                        {"label": "Option 2", "value": "OPTION_2"},
+                        {"label": "Option 3", "value": "OPTION_3"}
+                    ];
+                    break;
+                case "CHECKBOXES":
+                    field.label="New Checkboxes";
+                    field.options=[
+                        {"label": "Option A", "value": "OPTION_A"},
+                        {"label": "Option B", "value": "OPTION_B"},
+                        {"label": "Option C", "value": "OPTION_C"}
+                    ];
+                    break;
+                case "RADIOS":
+                    field.label="New Radio Buttons";
+                    field.options=[
+                        {"label": "Option X", "value": "OPTION_X"},
+                        {"label": "Option Y", "value": "OPTION_Y"},
+                        {"label": "Option Z", "value": "OPTION_Z"}
+                    ];
+                    break;
+            }
+            console.log(field);
+            vm.fields.push(field);
+        }
     }
 })();
