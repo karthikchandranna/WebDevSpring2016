df20a5cd5958cd529aa3006269d4c5e832eb21c1
karthikchandranna <karthik@ccs.neu.edu>
2016-03-19 00:04:32 -0400

    Assigment 3

--- public/assignment/server/models/form.model.js
+++ public/assignment/server/models/form.model.js
@@ -93,7 +93,6 @@ module.exports = function() {
 
     function sortFields(formId,fields){
         var form = findFormById(formId);
-        console.log(form);
         if(form){
             form.fields = fields;
             return updateForm(formId, form).fields;
